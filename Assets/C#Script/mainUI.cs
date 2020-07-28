46683a4308c25e9bc77bbad8f70596cdb4de3b2b
(유수빈) <(20201003@sungshin.ac.kr)>
2020-07-28 18:17:31 +0900

    새로운 프로젝트를 시작합니다.

---
+++ Assets/C#Script/mainUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BTNType
+{
+    GameStart,
+    Option,
+    Sound,
+    Back,
+    Quit,
+    Help,
+    Explain
+}
+
+public class mainUI : MonoBehaviour
+{
+
+    public void OnBtnClick()
+    {
+    }
+}
+
